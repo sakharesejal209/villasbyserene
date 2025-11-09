03e86ea1178ab90872a64afed5523b86375df752
sakharesejal209 <sakharesejal98@gmail.com>
2025-11-10 01:43:35 +0530

    added metadata to the location pages

--- src/app/stays/[slug]/page.tsx
+++ src/app/stays/[slug]/page.tsx
@@ -1,5 +1,40 @@
+import { Metadata } from "next";
 import StaysClientPage from "./StaysClientPage";
 
-export default function Page() {
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const villa = params.slug;
+
+  if (!villa) {
+    return {
+      title: "Villas near me | Villas by Serene",
+      description: "Near by villas found.",
+    };
+  }
+
+  return {
+    title: `Villas in ${villa} | Private Pool Villa in ${villa} | Villas by Serene`,
+    description: `Book a villa, a luxury stay in ${villa}.`,
+    openGraph: {
+      title: `Private ${villa} villa | Luxury Villa Stay`,
+      description: `Priavte pool ${villa} villa`,
+    },
+  };
+}
+
+export default async function Page({ params }: Readonly<PageProps>) {
+  const villa = params.slug;
+
+  if (!villa) {
+    return <div className="p-6 text-center text-gray-600">Villa not found</div>;
+  }
+
   return <StaysClientPage />;
 }
