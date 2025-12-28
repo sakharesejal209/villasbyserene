e64f4479862cb2b1f5f7c9fcb4266278c0d845d5
sakharesejal209 <sakharesejal98@gmail.com>
2025-12-28 17:14:26 +0530

    seo update

--- src/app/property/[slugAndId]/page.tsx
+++ src/app/property/[slugAndId]/page.tsx
@@ -1,8 +1,33 @@
 import * as React from "react";
 import { PrismaClient } from "../../../../generated/prisma";
 import Property from "@/app/components/property/Property";
+import { Metadata } from "next";
 
 const prisma = new PrismaClient();
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const { slug: location } = await params;
+
+  if (!location) {
+    return {
+      title: "Villas near me | Villas by Serene",
+      description: "Near by villas found.",
+    };
+  }
+
+  return {
+    title: `Villa in ${location} | Private Villa in ${location} | Private Pool Villa in ${location} | Villas by Serene`,
+    description: `Book a villa, a luxury stay in ${location}.`,
+    openGraph: {
+      title: `Private ${location} villa | Luxury Villa Stay`,
+      description: `Priavte pool ${location} villa`,
+    },
+  };
+}
 
 export default async function Page({
   params,
